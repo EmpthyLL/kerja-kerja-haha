ae4f33b670fda34a4218c106b57f4569fced6220
EmpthyLL <javileh40@gmail.com>
2024-10-06 18:22:39 +0700

    -..._

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
-import { ComboboxDemo } from "@/components/combobox"
+import { ComboboxDemo } from "@/components/combobox";
+import { Input } from "@/components/ui/input";
 
-export default function Dashboard(){
-    return <>
-    <h1>Dashboard</h1>
-    <ComboboxDemo/>
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <ComboboxDemo />
+      <Input maxLength={10} placeholder="Enter up to 10 characters" />
     </>
+  );
 }
